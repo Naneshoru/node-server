68b108e5322f5c02b845f783d1c0d0c70e80fc29
Ricardo Atakiama <ricardo_cssg@hotmail.com>
2025-01-21 14:07:08 -0300

    feat: servidor + rotas com fastify

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-  console.log('URL: ', req.url)
-  console.log('oi')
-  res.write('Brinks')
+// const server = http.createServer((req, res) => {
+//   console.log('URL: ', req.url)
+//   console.log('oi')
+//   res.write('Brinks')
 
-  return res.end()
+//   return res.end()
+// })
+
+// server.listen(3333, () => {
+//   console.log('Server is running on port 3333')
+// })
+
+
+import { fastify } from 'fastify'
+
+const server = fastify()
+
+server.get('/', async (request, reply) => {
+  console.log('Alou')
 })
 
-// porta para a aplicação executar
+server.get('/about', async (request, reply) => {
+  console.log('Sobre')
+})
 
-server.listen(3333, () => {
-  console.log('Server is running on port 3333')
+server.get('/node', async (request, reply) => {
+  console.log('Node')
+})
+
+server.listen({
+  port: 3333
 })
